8af449bc9f09f88c3d8c44dabb4288d0e2a71287
SimonEckerud <simoneckerud@gmail.com>
2021-01-20 11:26:36 +0100

    Initial commit

---
+++ Lab4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoopTill20();
+            LoopInput();
+        }
+
+        private static void LoopInput()
+        {
+            int x = 1;
+            Console.WriteLine("Skriv int ett tal mindre än 100: ");
+            int i =int.Parse(Console.ReadLine());
+            while(x <= i)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+
+        }
+
+        private static void LoopTill20()
+        {
+            int i = 1;
+            while(i <= 20)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
---
+++ Lab4Extra/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lab4Extra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoopTill20();
+            LoopInput();
+        }
+
+        private static void LoopInput()
+        {
+            int i = 1;
+            Console.WriteLine("Mata in ett nummer under 100: ");
+            int x = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i <= x);
+        }
+
+        private static void LoopTill20()
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i <= 20);
+            
+
+
+        }
+    }
+}
